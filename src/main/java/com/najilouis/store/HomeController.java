7727f6629274b082a793ef1ab7e795dae70f18da
Naji Louis <najilouis@gmail.com>
2025-11-13 13:36:37 -0500

    check devtools and use an added app property using @Value annotation

--- src/main/java/com/najilouis/store/HomeController.java
+++ src/main/java/com/najilouis/store/HomeController.java
@@ -1,12 +1,25 @@
 package com.najilouis.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.page-size}")
+    private int appPageSize;
+
     @RequestMapping("/")
     public String index() {
+//        String test = "hello";
+//        System.out.println(test);
+//        System.out.println("App Name: "+ appName);
+//        System.out.println("Page Size: "+ appPageSize);
+        System.out.println("App Name: "+ appName + " — " + "Page Size: "+ appPageSize);
         return "index.html";
     }
 
